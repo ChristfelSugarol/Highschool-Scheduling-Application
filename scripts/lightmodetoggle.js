b431824b16bc59c3c268c497258a71188cf41544
maxvstp <maxsirilan@gmail.com>
2025-02-05 00:16:31 +0800

    - Centered fixed schedule table elements. - Made light mode button text dynamic - Fixed some colors

--- scripts/lightmodetoggle.js
+++ scripts/lightmodetoggle.js
@@ -1,4 +1,12 @@
 function toggleLightMode() {
     var body = document.body;    
+    var button = document.querySelector("#navbar button[onclick='toggleLightMode()']");
+
     body.classList.toggle("light-mode");    
+
+    if (body.classList.contains("light-mode")) {
+        button.textContent = "Toggle Dark Mode";
+    } else {
+        button.textContent = "Toggle Light Mode";
+    }
 }
